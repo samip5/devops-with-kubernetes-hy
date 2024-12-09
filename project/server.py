eff2292a981ac37cf88879aea73d6da2dd061e22
Skyler Mäntysaari <sm+git@skym.fi>
2024-12-09 17:15:10 +0200

    project v0.3

--- project/server.py
+++ project/server.py
@@ -3,6 +3,10 @@ from os import environ
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "Hello world"
+
 if __name__ == '__main__':
     port = environ.get('PORT', 5000)
     app.run(port=port)
